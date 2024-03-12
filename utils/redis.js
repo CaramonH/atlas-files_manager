8bc2a9c8481bc2a01148e1d35edc84a1e8916cbe
CaramonH <caramonh@yahoo.com>
2024-03-12 13:40:43 -0500

    Task 0 - Redis utils passing in terminal

---
+++ utils/redis.js
@@ -0,0 +1,66 @@
+// Task 0 - Redis Utils
+
+import redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    // Display any errors in the console
+    this.client.on("error", (err) => {
+      console.error(`Redis client error: ${err}`);
+    });
+
+    // Connect to the server
+    this.client.connected = true;
+  }
+
+  // Check if the connection is alive
+  isAlive() {
+    // Check if the client is defined and connected
+    return !!(this.client && this.client.connected);
+  }
+
+  // Retrieve the value from Redis for the given key
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
+
+  // Store the value in Redis with expiration
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  // Remove the value from Redis for the given key
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
