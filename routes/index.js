9c2d24045a69ea2d6275012ded76b7e42ab58b3f
CaramonH <caramonh@yahoo.com>
2024-03-18 14:47:02 -0500

    Task 7 - first push

--- routes/index.js
+++ routes/index.js
@@ -29,5 +29,10 @@ router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 // This retrieves all files
 router.get('/files', FilesController.getIndex);
+// This publishes a file
+router.put('/files/:id/publish', FilesController.putPublish);
+// This unpublish a file
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
 
 module.exports = router;
